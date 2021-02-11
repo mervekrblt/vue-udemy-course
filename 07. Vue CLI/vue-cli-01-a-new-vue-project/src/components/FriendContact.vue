c8dcec7c7d1e49e3cea547eecd77ca45934da9dc
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-02-11 23:43:20 +0300

    Add vue cli examples

---
+++ 07. Vue CLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data() {
+        return {
+            isVisible: false,
+            friend: {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        }
+        }
+    },
+
+    methods: {
+        toggleDetails() {
+            this.isVisible = !this.isVisible
+        }
+    },
+}
+</script>
+
+<template>
+
+<li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+
+    <ul v-if="isVisible">
+        <li> {{ friend.phone }} </li>
+        <li> {{ friend.email }} </li>
+    </ul>
+</li>
+</template>
