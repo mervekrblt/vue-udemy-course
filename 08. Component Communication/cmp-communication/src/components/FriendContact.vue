19388abb90bb66088839c8a75ea0dbfd8119d9bf
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-06 00:29:12 +0300

    Add 8th lecture

---
+++ 08. Component Communication/cmp-communication/src/components/FriendContact.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  // props: [
+  //   'name',
+  //   'phone',
+  //   'email',
+  //   'isFavorite'
+  // ],
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+  },
+
+  data() {
+    return {
+      detailsAreVisible: false,
+      // FOR MUTATE PROP
+      friendFavorite: this.isFavorite
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+
+    addFavorite() {
+      // CANNOT MUTATE PROPS
+      // if(this.isFavorite === '1') {
+      //   this.isFavorite = 0
+      // }
+
+      // if(this.friendFavorite === '1') {
+      //   this.friendFavorite = '0'
+      // } else {
+      //   this.friendFavorite = '1'
+      // }
+
+      this.friendFavorite = !this.friendFavorite;
+    }
+  }
+};
+</script>
+
+<template>
+  <li>
+    <h2>{{ name }} {{ friendFavorite ? '(Favorite)' : '' }} </h2>
+    <button @click="addFavorite">Add Favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
