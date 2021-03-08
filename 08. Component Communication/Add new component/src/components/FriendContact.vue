10bf777ad1fb20adf826b3590d889396422c1374
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-08 18:06:39 +0300

    Add new component to add new contact

---
+++ 08. Component Communication/Add new component/src/components/FriendContact.vue
@@ -0,0 +1,81 @@
+<script>
+export default {
+  // props: [
+  //   'name',
+  //   'phone',
+  //   'email',
+  //   'isFavorite'
+  // ],
+  emit: ['toggle-favorite'],
+
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+    },
+  },
+
+  data() {
+    return {
+      detailsAreVisible: false,
+      // FOR MUTATE PROP
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+
+    addFavorite() {
+      // CANNOT MUTATE PROPS
+      // if(this.isFavorite === '1') {
+      //   this.isFavorite = 0
+      // }
+
+      // if(this.friendFavorite === '1') {
+      //   this.friendFavorite = '0'
+      // } else {
+      //   this.friendFavorite = '1'
+      // }
+
+      //this.friendFavorite = !this.friendFavorite;
+
+      this.$emit('toogle-favorite', this.id)
+    }
+  }
+};
+</script>
+
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : '' }} </h2>
+    <button @click="addFavorite">Add Favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
---
+++ 08. Component Communication/Add new component/src/components/NewFriend.vue
@@ -0,0 +1,66 @@
+<script>
+export default {
+    data () {
+        return {
+            name: '',
+            phone: '',
+            email: '',
+        }
+    },
+
+    emits: ['add-contact'],
+
+    methods: {
+        submitData () {
+            const data = {
+                name: this.name,
+                phone: this.phone,
+                email: this.email
+            }
+            //console.log(data)
+            this.$emit('add-contact', data)
+        }
+    }
+}
+</script>
+
+
+<template>
+<form @submit.prevent="submitData">
+    <div>
+        <label>Name</label>
+        <input type="text" v-model="name">
+    </div>
+
+    <div>
+        <label>Phone</label>
+        <input type="tel" v-model="phone">
+    </div>
+
+    <div>
+        <label>Email</label>
+        <input type="text" v-model="email">
+    </div>
+
+    <div>
+        <button>Submit</button>
+    </div>
+</form>
+</template>
+
+
+<style>
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+</style>
