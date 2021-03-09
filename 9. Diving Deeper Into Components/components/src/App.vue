a2bc940bb0ff3289c742f338592c6bfa3cf1f7e7
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-09 13:20:19 +0300

    Initial commit

---
+++ 9. Diving Deeper Into Components/components/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      v-for="user in activeUser"
+      :key="user.name"
+      :full-name="user.name"
+      :info-text="user.description"
+      :role="user.role"
+      :phone="user.phone"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeUser: [
+        {
+          name: 'Maximilian Schwarzmüller',
+          phone: 123,
+          description: 'Site owner and admin',
+          role: 'admin',
+        },
+        {
+          name: 'Merve Karabulut',
+          phone: 123,
+          description: 'Site owner and admin',
+          role: 'admin',
+        }
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
