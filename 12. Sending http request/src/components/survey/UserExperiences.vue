ca46d2ddda4b9c6f1f725dd098907e42755409e7
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-24 12:13:26 +0300

    Add 12nd exercise and add border to understand components

---
+++ 12. Sending http request/src/components/survey/UserExperiences.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button>Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  props: ['results'],
+  components: {
+    SurveyResult,
+  },
+};
+</script>
+
+<style scoped>
+section {
+  border: 2px green solid;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
