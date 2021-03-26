3f7e6eef14a71bfd86bdb246e5a7d5be1a5199e5
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-26 13:57:02 +0300

    Add routes and page components

---
+++ 16. Find a Coach Web App/src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachDetails from './pages/coaches/CoachDetails'
+import CoachesList from './pages/coaches/CoachesList'
+import CoachRegistration from './pages/coaches/CoachRegistration'
+import ContactCoach from './pages/request/ContactCoach'
+import RequestReceived from './pages/request/RequestReceived'
+import NotFound from './pages/NotFound'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetails,
+      children: [{ path: '/contact', component: ContactCoach }],
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default router;
