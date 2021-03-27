2218cd885059c8dd778bc38119af0fb767fd5644
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-27 22:35:25 +0300

    Add requestreceived component. Show all requests

--- 16. Find a Coach Web App/src/pages/request/RequestReceived.vue
+++ 16. Find a Coach Web App/src/pages/request/RequestReceived.vue
@@ -1,3 +1,48 @@
 <template>
-<h1>Request</h1>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="req in receivedRequest"
+          :key="req.id"
+          :message="req.message"
+          :email="req.userEmail"
+        ></request-item>
+      </ul>
+      <h3 v-else>No requests</h3>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+export default {
+  components: { RequestItem },
+  computed: {
+    receivedRequest() {
+      console.log('req', this.$store.getters['requests/requests']);
+      return this.$store.getters['requests/requests'];
+    },
+
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+</style>
