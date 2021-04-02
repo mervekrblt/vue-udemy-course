2550237bfb53fcd1047cea166167f29816c32f6d
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-04-02 16:05:59 +0300

    Add global mixin

--- 20. Reusing Functionality Mixins/src/main.js
+++ 20. Reusing Functionality Mixins/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import logginMixin from './mixins/logger'
 
 const app = createApp(App)
 
+app.mixin(logginMixin)
 app.mount('#app');
